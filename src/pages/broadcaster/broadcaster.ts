c19f17b320326b818995c391c88a93b1c1ff59b7
Tom Sundstrom <office@tomsun.ax>
2018-05-24 10:43:07 +0300

    Let page animations finish before using alert

--- src/pages/broadcaster/broadcaster.ts
+++ src/pages/broadcaster/broadcaster.ts
@@ -28,13 +28,17 @@ export class BroadcasterPage {
     });
   }
 
-  ionViewDidEnter() {
-    if (APPLICATION_ID === 'CHANGEME') alert('Warning: APPLICATION_ID is not set. Get your application id at https://dashboard.irisplatform.io/developer and update pages/broadcaster/broadcaster.ts, then rebuild the app.');
+  async ionViewDidEnter() {
+    if (APPLICATION_ID === 'CHANGEME') {
+      await new Promise(resolve => setTimeout(resolve, 500)); // Let page animations to finish before using alert()
+      alert('Warning: APPLICATION_ID is not set. Get your application id at https://dashboard.irisplatform.io/developer and update pages/broadcaster/broadcaster.ts, then rebuild the app.');
+    }
 
     // Engage our Ionic CSS background overrides that ensure viewfinder is visible.
     document.getElementsByTagName('body')[0].classList.add("show-viewfinder");
 
     if (!this.broadcaster) {
+      await new Promise(resolve => setTimeout(resolve, 500)); // Let page animations to finish before using alert()
       alert('broadcaster is not ready yet');
       return;
     }
